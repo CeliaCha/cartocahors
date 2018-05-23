e7d8b507d238efdd074685b096e26bf48b8ff135
echayotte <echayotte@gmail.com>
2018-05-23 15:58:35 +0200

    refacto templ, chgt name/id, chgt data/item.children. Supp name, methods

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,40 +1,51 @@
 <template>
-    <div>
-        <div v-for="item in list" :key="item.name" class="category">
-            <b-btn v-b-toggle="item.name" class="m-1">{{ item.name }}</b-btn>
-            <b-collapse v-bind:id="item.name">
-                <Subcategorie :data="item"/>
+
+    <div class="col-md-3">
+        <div v-for="item in list" :key="item.id">
+
+            <b-btn v-b-toggle="item.id" variant="primary" class="btn-block m-1 button text-left">
+                {{ item.name }}
+            </b-btn>
+            
+            <b-collapse v-bind:id="item.id">
+                <Subcategorie :data="item.children"/>
             </b-collapse>
+
         </div>
     </div>
+    
 </template>
 
+
 <script>
-import Subcategorie from './Subcategorie'
-import json from "../assets/places.json"
 
-export default {
-  name: "Menu",
-  components : {
-      Subcategorie
-  },
-  data() {
-    return {
-      list: json
+    import Subcategorie from './Subcategorie'
+    import json from "../assets/places.json"
+
+    export default {
+        components : {
+            Subcategorie
+        },
+
+        data() {
+            return {
+                list: json
+            }
+        },
+
     }
-  },
-  mounted() {
-
-  },
-  computed: {
-    
-  }
-}
 
 </script>
 
+
 <style scoped>
-    b-btn {
-        min-width: 300px;
+    
+    button {
+        font-weight: bolder;
+        font-family: 'Quicksand', sans-serif;
     }
+    button::first-letter {
+        text-transform: uppercase;
+    }
+
 </style>
