2c60a320e8a3b38e8e2f0544f391f782479132ec
echayotte <echayotte@gmail.com>
2018-05-23 15:53:15 +0200

    refactorisation en div bootstrap et suppr style. Indentation et aération

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
-    <div id="app">
-      <Menu/>
-      <CityMap/>
+
+    <div class="container-fluid" >
+        <div class="row">
+
+            <Menu/>
+            <CityMap/>
+
+        </div>
     </div>
+
 </template>
 
+
 <script>
-import Menu from './components/Menu'
-import CityMap from './components/CityMap'
+    import Menu from './components/Menu'
+    import CityMap from './components/CityMap'
 
+    export default {
+        
+        name: 'App',
+        components: {
+            Menu,
+            CityMap
+        },
 
-export default {
-  name: 'App',
-      components: {
-      Menu,
-      CityMap
-    },
+    }
 
-}
 </script>
 
 
 <style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-        margin-top: 60px;
-    }
+
 </style>
