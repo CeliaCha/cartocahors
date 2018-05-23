a5fbfbadd2b70ac5aae2d9a5b4719de03fad5f6c
echayotte <echayotte@gmail.com>
2018-05-23 11:12:32 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,28 @@
 <template>
-  <div id="app">
-    Hello world
-  </div>
+    <div id="app">
+        <CityMap/>
+    </div>
 </template>
 
 <script>
+    import CityMap from './components/CityMap'
 
-export default {
-  name: 'App',
-}
+    export default {
+        name: 'App',
+        components: {
+            CityMap,
+        }
+    }
 </script>
 
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
---
+++ src/components/CityMap.vue
@@ -0,0 +1,65 @@
+<template>
+
+    <div id="general" class="container">
+        <div class="row">
+            
+            <div class="col-md-12">
+                <div id="map" class="map"></div>
+            </div>
+
+       </div>
+    </div>
+
+</template>
+
+
+<script>
+
+    // voir tuto : https://travishorn.com/interactive-maps-with-vue-leaflet-5430527353c8
+    export default {
+        name: '#general',
+
+        /* Data properties will go here */
+        data: {
+            map: null,
+            tileLayer: null,
+            layers: [],
+        },
+
+        /* Code to run when app is mounted */
+        mounted() {
+            this.initMap();
+            this.initLayers();          },
+
+        /* Any app-specific functions go here */
+        methods: {
+
+            initMap() {
+                
+                this.map = L.map('map').setView([38.63, -90.23], 12);
+
+                this.tileLayer = L.tileLayer(
+                    'https://cartodb-basemaps-{s}.global.ssl.fastly.net/rastertiles/voyager/{z}/{x}/{y}.png',
+                    {
+                        maxZoom: 18,
+                        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, &copy; <a href="https://carto.com/attribution">CARTO</a>',
+                    }
+                );
+
+                this.tileLayer.addTo(this.map);
+            },
+
+            initLayers() {},
+        },
+    }
+
+</script>
+
+
+<style scoped>
+
+    .map {
+        height: 90vh;
+    }
+
+</style>
